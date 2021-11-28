21856b82e753c78400d4a9386a6869f885a38ca4
Jose-Eduardo-PC <joseduardopati@gmail.com>
2021-11-28 19:07:06 -0300

    login con facebook final 9

--- routes/admin.php
+++ routes/admin.php
@@ -3,4 +3,4 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\HomeController;
 
-Route::get('', [HomeController::class, 'index']);
+Route::get('admin', [HomeController::class, 'index'])->name('admin');
