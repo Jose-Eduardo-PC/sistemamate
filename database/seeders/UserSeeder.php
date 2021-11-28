c6f4179995bd3116f2f21d5a85b427ee8a16d5f2
Jose-Eduardo-PC <joseduardopati@gmail.com>
2021-11-28 18:58:09 -0300

    login con facebook final 7

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,7 +2,7 @@
 
 namespace Database\Seeders;
 
-use App\Models\user;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class UserSeeder extends Seeder
@@ -14,6 +14,6 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        user::factory(100)->create();
+        User::factory(100)->create();
     }
 }
