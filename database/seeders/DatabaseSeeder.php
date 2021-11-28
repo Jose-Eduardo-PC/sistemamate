468c10c0aa83c890736502ffe1fa887c7497170f
Jose-Eduardo-PC <joseduardopati@gmail.com>
2021-11-28 18:54:29 -0300

    login con facebook final 6

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,6 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
-use App\Models\user;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,17 +14,15 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
-        $user = new user();
-        
-$this->call(RolSeeder::class);  
-$this->call(UserSeeder::class);
-$this->call(CursoSeeder::class);
-$this->call(AsistenciaSeeder::class);
-$this->call(MateriaSeeder::class);
-$this->call(TemaSeeder::class);
-$this->call(EvaluacionSeeder::class);
-$this->call(ActividadSeeder::class);
-$this->call(User_cursoSeeder::class);
 
+        $this->call(RolSeeder::class);
+        $this->call(UserSeeder::class);
+        $this->call(CursoSeeder::class);
+        $this->call(AsistenciaSeeder::class);
+        $this->call(MateriaSeeder::class);
+        $this->call(TemaSeeder::class);
+        $this->call(EvaluacionSeeder::class);
+        $this->call(ActividadSeeder::class);
+        $this->call(User_cursoSeeder::class);
     }
 }
